1c576b4ea8c47b2c66d67a1439bbdf94d504e7c6
s1387tnt <s1387tnt@gmail.com>
2024-05-17 17:05:07 +0800

    feat(product): 期末卡片樣式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, ProductCardComponent],
 })
 export class AppComponent {
   title = 'mcu-112-2-web-12131290-final';
