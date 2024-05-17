a052c9ddff459d0e778f9528dd6798d6dc524a30
s1387tnt <s1387tnt@gmail.com>
2024-05-17 17:18:19 +0800

    feat(product): 利用文件插値顯示產品名稱、作者與公司資訊

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  productName = 'A產品';
+  authors = '作者A、作者B、作者C';
+  company = '博碩文化';
+}
