e539cb179250771fff2c3aefb81da227c1bce21d
s1387tnt <s1387tnt@gmail.com>
2024-05-26 00:11:08 +0800

    feat(product-detail): 切換產品明細頁面實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ShoppingCartPageComponent } from './shopping-cart-page/shopping-cart-page.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products' },
   { path: 'products', component: ProductPageComponent },
+  { path: 'product/:name', component: ProductDetailPageComponent },
   { path: 'shopping-cart', component: ShoppingCartPageComponent },
 ];
