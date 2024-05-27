c70a29936d97e9e5daf2c86ac7f7eff2fb3ab4e1
s1387tnt <s1387tnt@gmail.com>
2024-05-27 20:12:00 +0800

    feat(product-detail): 產品明細頁面切版實作

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-product-detail-page',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-detail-page.component.html',
-  styleUrl: './product-detail-page.component.css'
+  styleUrl: './product-detail-page.component.css',
 })
 export class ProductDetailPageComponent {
+  product = new Product({
+    name: 'A產品',
+    authors: ['作者A', '作者B', '作者C'],
+    company: '博碩文化',
+    imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    price: 1580,
+  });
 
+  private router = inject(Router);
+
+  onBack(): void {
+    this.router.navigate(['products']);
+  }
 }
