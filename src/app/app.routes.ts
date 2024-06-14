b0f96af64eea6b69df610e8797b37d5fa14c9780
s1387tnt <s1387tnt@gmail.com>
2024-06-15 01:43:59 +0800

    feat(resolver): 建立產品資料預戴資料

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { productResolver } from './resolver/product.resolver';
 import { ShoppingCartPageComponent } from './shopping-cart-page/shopping-cart-page.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products' },
   { path: 'products', component: ProductPageComponent },
-  { path: 'product/:id', component: ProductDetailPageComponent },
+  {
+    path: 'product/:id',
+    component: ProductDetailPageComponent,
+    resolve: { product: productResolver },
+  },
   { path: 'shopping-cart', component: ShoppingCartPageComponent },
 ];
