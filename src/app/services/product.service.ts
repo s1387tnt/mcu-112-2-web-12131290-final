0987c09670f585572182f71004cc20af39c29fb2
s1387tnt <s1387tnt@gmail.com>
2024-06-15 02:35:54 +0800

    feat(pagination): 新增分頁元件

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -43,6 +43,15 @@ export class ProductService {
       imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
       price: 1580,
     }),
+    new Product({
+      id: 5,
+      name: 'E產品',
+      authors: ['作者A', '作者B', '作者C'],
+      company: '博碩文化',
+      isSale: false,
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
   ];
 
   getList(): Observable<Product[]> {
