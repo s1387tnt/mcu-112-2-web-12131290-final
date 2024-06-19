96432237d56fd95425f00bdde9128af4521bc1d7
s1387tnt <s1387tnt@gmail.com>
2024-06-19 18:11:35 +0800

    fix(shoppint-cart): 購買項目修改

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -12,6 +12,7 @@ export class ProductService {
       name: 'A產品',
       authors: ['作者A', '作者B', '作者C'],
       company: '博碩文化',
+      isShow: true,
       isSale: true,
       imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
       price: 1580,
